845d081f87437e560f9bb5509f51bbbbd07f73cd
ltaverasG <64043686+ltaverasG@users.noreply.github.com>
2021-09-29 23:17:03 -1100

    condicionales

---
+++ condicionales.py
@@ -0,0 +1,15 @@
+## condicionales de python hay que tener encuenta la identacion  
+x = 10 
+
+if x < 20: 
+    print("x es menor a 20")
+else:
+    print("x es mayor que 20 ")
+
+
+# tambien condicionales con operadores logico  AND OR NOT 
+if x > 2 and x < 10:
+    print ('numero mayor a 2 y menor a 10')
+
+if x > 2 or  x <= 20:
+    print(' x mayor a dos pero menor o igual a 20 ')    
